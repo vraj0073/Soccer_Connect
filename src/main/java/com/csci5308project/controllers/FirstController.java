14214bc96d7b9cc49a996a021496b0a2389e931b
vraj jadhav <vr880417@dal.ca>
2021-10-22 17:29:18 -0300

    HtML page working

--- src/main/java/com/csci5308project/controllers/FirstController.java
+++ src/main/java/com/csci5308project/controllers/FirstController.java
@@ -1,16 +1,18 @@
 package com.csci5308project;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@RestController
+@Controller
 public class FirstController {
     
-    @GetMapping(path = "/first")
+    @GetMapping(value = "/first")
     public String initializeApp(){
-        return "Hello World.";
+        return "firstpage";
     }
 
 }
