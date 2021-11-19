7d39f1f5f7cdf87942fd16743ecbafc5c1d6c0f5
Vishwanath Hosdurga Suresh <vs542176@dal.ca>
2021-11-19 16:08:51 -0400

    Password hashing

--- src/main/java/com/soccerconnect/controllers/LoginController.java
+++ src/main/java/com/soccerconnect/controllers/LoginController.java
@@ -1,6 +1,8 @@
 package com.soccerconnect.controllers;
 
 
+import com.soccerconnect.models.LoginModel;
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController extends MasterController{
+
+    LoginModel lm = new LoginModel();
 
     @GetMapping(value = "/")
     public String index()
@@ -26,16 +30,20 @@ public class LoginController extends MasterController{
     public String loginSubmit(    @RequestParam(value = "email") String email,
                                   @RequestParam(value = "password") String password) {
 
-        MasterController.currentUserId = rm.getUserId(email);
-        int RoleId = rm.getRoleFromEmail(email);
-        if (RoleId == 0) {
-            return "welcomeAdmin";
-        } else if (RoleId == 1) {
-            return "welcomePlayer";
-        } else if (RoleId == 2){
-            return "welcomeTeam";
-        }else{
-            return "login";
+        String passwordDb = lm.getPasswordFromEmail(email);
+        if (BCrypt.checkpw(password, passwordDb)) {
+            MasterController.currentUserId = rm.getUserId(email);
+            int RoleId = rm.getRoleFromEmail(email);
+            if (RoleId == 0) {
+                return "welcomeAdmin";
+            } else if (RoleId == 1) {
+                return "welcomePlayer";
+            } else if (RoleId == 2) {
+                return "welcomeTeam";
+            } else {
+                return "login";
+            }
         }
+        else return "login";
     }
 }
