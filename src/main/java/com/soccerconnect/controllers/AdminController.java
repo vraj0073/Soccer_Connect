d67ece6a097d8c9c574338f8aee3425c419278b8
Vishwanath Hosdurga Suresh <vs542176@dal.ca>
2021-11-30 11:10:48 -0400

    Refactoring some code

--- src/main/java/com/soccerconnect/controllers/AdminController.java
+++ src/main/java/com/soccerconnect/controllers/AdminController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 
 
