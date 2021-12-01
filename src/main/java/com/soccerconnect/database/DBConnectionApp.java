4f5f64c7d47a869349124c39114eae4d17dcd14e
Harsh Patel <hr767477@dal.ca>
2021-12-01 14:10:10 -0400

    db connection from config file

--- src/main/java/com/soccerconnect/database/DBConnectionApp.java
+++ src/main/java/com/soccerconnect/database/DBConnectionApp.java
@@ -1,20 +1,25 @@
 package com.soccerconnect.database;
 
+import com.soccerconnect.utils.ConfigReader;
+import java.io.IOException;
 import java.sql.*;
+import java.util.HashMap;
 
 public class DBConnectionApp {
-
-    String HOST = "db-5308.cs.dal.ca";
-    String SCHEMA = "CSCI5308_15_TEST";
-    String USER = "CSCI5308_15_TEST_USER";
-    String PASSWORD = "ooquiekieRoo5nah";
-    String URL = "jdbc:mysql://" + HOST + ":3306/" + SCHEMA;
+    
     public Connection conn;
 
     public DBConnectionApp() {
         try {
+            ConfigReader properties = new ConfigReader();
+            HashMap<String,String> configuration = properties.getPropValues();
+            String HOST = configuration.get("HOST");
+            String SCHEMA = configuration.get("SCHEMA");
+            String USER = configuration.get("USER");
+            String PASSWORD = configuration.get("PASSWORD");
+            String URL = "jdbc:mysql://" + HOST + ":3306/" + SCHEMA;
             conn = DriverManager.getConnection(URL, USER, PASSWORD);
-        } catch (SQLException e) {
+        } catch (SQLException | IOException e) {
             System.out.println(e);
         }
     }
