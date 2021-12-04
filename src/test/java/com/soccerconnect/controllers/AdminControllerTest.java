f41d90d3d38f49cc2ecb9cd005e93e071760b00d
vraj jadhav <vr880417@dal.ca>
2021-12-04 18:41:08 -0400

    score game added

---
+++ src/test/java/com/soccerconnect/controllers/AdminControllerTest.java
@@ -0,0 +1,20 @@
+package com.soccerconnect.controllers;
+
+import com.soccerconnect.models.StatsModel;
+import org.junit.jupiter.api.Test;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AdminControllerTest {
+
+
+    @Test
+    void validationScore() {
+    }
+
+    @Test
+    void validationMOM() {
+    }
+}
